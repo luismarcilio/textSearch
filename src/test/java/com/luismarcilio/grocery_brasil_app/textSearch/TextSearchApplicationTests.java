ae1947895eed3a3a1fff38c95d0210788b25723e
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2021-03-24 08:36:52 -0300

    first commit

---
+++ src/test/java/com/luismarcilio/grocery_brasil_app/textSearch/TextSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luismarcilio.grocery_brasil_app.textSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
