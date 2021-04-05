00070a574ce0e3969a70a738ee277a6281500281
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2021-04-05 12:03:18 -0300

    feat: add beans Analyzer and SearcherManager

--- src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/config/LuceneConfig.java
+++ src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/config/LuceneConfig.java
@@ -10,6 +10,7 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.search.SearcherManager;
 import org.apache.lucene.store.Directory;
 import static org.apache.lucene.store.NIOFSDirectory.open;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,11 +24,11 @@ public class LuceneConfig {
     private final Analyzer analyzer;
     private final IndexWriterConfig indexWriterConfig;
     private final IndexWriter indexWriter;
-    
+
 
     @Autowired
-    public LuceneConfig(DirectoryConfig directoryConfig) throws IOException, URISyntaxException{
-        this.directory = open(Paths.get(new URI(directoryConfig.getPath())));
+    public LuceneConfig(DirectoryPath directoryPath) throws IOException, URISyntaxException{
+        this.directory = open(Paths.get(new URI(directoryPath.getPath())));
         this.analyzer = new StandardAnalyzer();
         this.indexWriterConfig = new IndexWriterConfig(analyzer);
         this.indexWriter = new IndexWriter(this.directory, this.indexWriterConfig);
@@ -37,7 +38,19 @@ public class LuceneConfig {
     public Directory getDirectory(){
         return this.directory;
     }
-    @Bean IndexWriter getIndexWriter(){
+    @Bean 
+    public IndexWriter getIndexWriter(){
         return this.indexWriter;
     }
+
+    @Bean  
+    public Analyzer getAnalyzer(){
+        return this.analyzer;
+    }
+
+    @Bean 
+    SearcherManager getSearcherManager() throws IOException{        
+        return new SearcherManager(indexWriter,null);
+    }
+
 }
