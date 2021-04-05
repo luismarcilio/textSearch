8dcd2f97f1775783b973bbd3718aa2efc45eb63e
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2021-04-05 13:53:44 -0300

    fix: match model

--- src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/domain/DocumentFromProduct.java
+++ src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/domain/DocumentFromProduct.java
@@ -2,19 +2,23 @@ package com.luismarcilio.grocery_brasil_app.textSearch.domain;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StoredField;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 
 public class DocumentFromProduct implements DocumentFromEntity<Product>{
 
-    
     @Override
     public Document getDocument(Product product) {
         Document document = new Document();
-        document.add(new Field("id", product.getId(), StringField.TYPE_STORED));
-        document.add(new Field("name", product.getName(), TextField.TYPE_STORED));
-        document.add(new Field("eanCode", product.getEanCode(), StringField.TYPE_STORED));
-        document.add(new Field("thumbnail", product.getThumbnail(), StringField.TYPE_STORED));
+        document.add(new StringField("id", product.getId(), Field.Store.YES));
+        document.add(new TextField("name", product.getName(), Field.Store.YES));
+        document.add(new StoredField("eanCode", product.getEanCode()));
+        document.add(new StoredField("ncmCode", product.getNcmCode()));
+        document.add(new StoredField("normalized", product.isNormalized()?"true":"false"));
+        document.add(new StoredField("thumbnail", product.getThumbnail()));
+        document.add(new StoredField("normalizationStatus", product.getNormalizationStatus()));
+        document.add(new StoredField("unity.name", product.getUnity().getName()));
         return document;
     }
 
--- src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/domain/ProductFromDocument.java
+++ src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/domain/ProductFromDocument.java
@@ -2,18 +2,18 @@ package com.luismarcilio.grocery_brasil_app.textSearch.domain;
 
 import org.apache.lucene.document.Document;
 
-public class ProductFromDocument implements EntityFromDocument<Product>{
+public class ProductFromDocument implements EntityFromDocument<Product> {
 
     @Override
     public Product getEntity(Document document) {
-        Product product = new Product();
-        product.setId(document.getField("id").stringValue());
-        product.setEanCode(document.getField("eanCode").stringValue());
-        product.setName(document.getField("name").stringValue());
-        product.setThumbnail(document.getField("thumbnail").stringValue());
-        return product;
-        
-
+        return Product.builder()
+            .id(document.getField("id").stringValue())
+            .name(document.getField("name").stringValue())
+            .eanCode(document.getField("eanCode").stringValue()).ncmCode(document.getField("ncmCode").stringValue())
+            .normalized(Boolean.parseBoolean(document.getField("normalized").stringValue()))
+            .thumbnail(document.getField("thumbnail").stringValue())
+            .normalizationStatus(Integer.parseInt(document.getField("normalizationStatus").stringValue()))
+            .unity(Unity.builder().name(document.getField("unity.name").stringValue()).build()).build();
     }
-    
+
 }
