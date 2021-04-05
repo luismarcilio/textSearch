6968cb1d0fed3de40ba3a0ad19c521c28bf2b637
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2021-04-05 13:53:20 -0300

    feat: add builder

--- src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/domain/Product.java
+++ src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/domain/Product.java
@@ -1,16 +1,19 @@
 package com.luismarcilio.grocery_brasil_app.textSearch.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Product {
     private String id;
     private String name;
     private String eanCode;    
+    private String ncmCode;    
+    private Unity unity;    
     private boolean normalized;
     private String thumbnail;
     private int normalizationStatus;    
--- src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/domain/Unity.java
+++ src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/domain/Unity.java
@@ -1,12 +1,14 @@
 package com.luismarcilio.grocery_brasil_app.textSearch.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Unity {
     private String name;
 }
