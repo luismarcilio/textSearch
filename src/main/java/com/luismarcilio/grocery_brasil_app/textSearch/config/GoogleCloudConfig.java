b597639eda5a6a4eef21678d9a08fc3ee9f7bf4a
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2021-04-06 17:22:38 -0300

    fix: logging the credentials

--- src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/config/GoogleCloudConfig.java
+++ src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/config/GoogleCloudConfig.java
@@ -2,21 +2,17 @@ package com.luismarcilio.grocery_brasil_app.textSearch.config;
 
 import java.io.IOException;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.extern.slf4j.Slf4j;
 
 @Configuration
-@Slf4j
 public class GoogleCloudConfig {
 
     @Bean 
     SecretManagerServiceClient getSecretManagerServiceClient() throws IOException{
-        log.info("Google Credentials: {}", GoogleCredentials.getApplicationDefault());
         return SecretManagerServiceClient.create();
     }
     
--- src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/config/GoogleCloudSecret.java
+++ src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/config/GoogleCloudSecret.java
@@ -2,12 +2,20 @@ package com.luismarcilio.grocery_brasil_app.textSearch.config;
 
 import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse;
 import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
+import com.luismarcilio.grocery_brasil_app.textSearch.repository.ProductRepositoryRuntimeException;
+
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.secretmanager.v1.*;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 @EnableConfigurationProperties(GoogleCloudProperties.class)
 public class GoogleCloudSecret {
@@ -22,6 +30,11 @@ public class GoogleCloudSecret {
     
     @Bean(name="ApiKey")
     public String getApiKey(){
+        try {
+            log.info("Google Credentials: {}", GoogleCredentials.getApplicationDefault());
+        } catch (IOException e) {
+            throw new ProductRepositoryRuntimeException(e);
+        }
         AccessSecretVersionResponse response = client.accessSecretVersion(SecretVersionName.of(this.projectId, this.secretId, "latest"));
         return response.getPayload().getData().toStringUtf8();
     }
