31e67139b38c7392472ea45568ffb89748a37628
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2021-04-06 17:20:44 -0300

    feat: cross cutting debug annotation

---
+++ src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/LoggerAspect.java
@@ -0,0 +1,35 @@
+package com.luismarcilio.grocery_brasil_app.textSearch;
+
+import java.util.stream.Stream;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggerAspect {
+
+    @Around("@annotation(com.luismarcilio.grocery_brasil_app.textSearch.WithLog)")
+    public Object doLog(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        StringBuilder stringBuilder = new StringBuilder();
+        try{
+            stringBuilder.append(proceedingJoinPoint.getSignature().getName()+"(");
+            Stream.of(proceedingJoinPoint.getArgs()).forEach(arg -> stringBuilder.append(arg.toString()+","));
+            stringBuilder.append("): ");
+            final Object proceed = proceedingJoinPoint.proceed();
+            if(proceed != null){
+                stringBuilder.append(proceed.toString());
+            }
+            return proceed;            
+        }
+        finally{
+            log.debug(stringBuilder.toString());
+        }
+    }
+    
+}
---
+++ src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/WithLog.java
@@ -0,0 +1,9 @@
+package com.luismarcilio.grocery_brasil_app.textSearch;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface WithLog {
+    
+}
