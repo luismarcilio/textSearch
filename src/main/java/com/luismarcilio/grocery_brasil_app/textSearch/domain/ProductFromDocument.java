377e5a36db8f913fc8c863a88fbe6d3ac41a5cb7
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2021-04-07 12:28:31 -0300

    fix: null aware

--- src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/domain/ProductFromDocument.java
+++ src/main/java/com/luismarcilio/grocery_brasil_app/textSearch/domain/ProductFromDocument.java
@@ -10,13 +10,14 @@ public class ProductFromDocument implements EntityFromDocument<Product> {
 	@WithLog
     public Product getEntity(Document document) {
         return Product.builder()
-            .id(document.getField("id").stringValue())
-            .name(document.getField("name").stringValue())
-            .eanCode(document.getField("eanCode").stringValue()).ncmCode(document.getField("ncmCode").stringValue())
-            .normalized(Boolean.parseBoolean(document.getField("normalized").stringValue()))
-            .thumbnail(document.getField("thumbnail").stringValue())
-            .normalizationStatus(Integer.parseInt(document.getField("normalizationStatus").stringValue()))
-            .unity(Unity.builder().name(document.getField("unity.name").stringValue()).build()).build();
+            .id(document.getField("id")==null?null:document.getField("id").stringValue())
+            .name(document.getField("name")==null?null:document.getField("name").stringValue())
+            .eanCode(document.getField("eanCode")==null?null:document.getField("eanCode").stringValue())
+            .ncmCode(document.getField("ncmCode")==null?null:document.getField("ncmCode").stringValue())
+            .normalized(Boolean.parseBoolean(document.getField("normalized")==null?"false":document.getField("normalized").stringValue()))
+            .thumbnail(document.getField("thumbnail")==null?null:document.getField("thumbnail").stringValue())
+            .normalizationStatus(Integer.parseInt(document.getField("normalizationStatus")==null?"0":document.getField("normalizationStatus").stringValue()))
+            .unity(Unity.builder().name(document.getField("unity.name")==null?null:document.getField("unity.name").stringValue()).build()).build();
     }
 
 }
